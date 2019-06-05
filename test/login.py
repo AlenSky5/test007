dd76c6401cab2d2787732c42f883149b4c883148
Alen <xwp_fullstack@163.com>
2019-06-05 19:03:25 +0800

    Alen

--- test/login.py
+++ test/login.py
@@ -1,6 +1,6 @@
 print('hello world!')
 print('hello world!')
-<<<<<<< HEAD
+
 print('hello manage,my name is Alen!')
 print('hello manage,my name is Alen!')
 print('hello manage,my name is Alen!')
@@ -10,11 +10,11 @@ print('hello manage,my name is Alen!')
 print('hello manage,my name is Alen!')
 print('hello manage,my name is Alen!')
 print('hello manage,my name is Alen!')
-=======
+
 print('hello world!')
 print('hello world!')
 print('hello world!')
->>>>>>> e186abc0b83494d0297562425f6cef4d013f48eb
+
 print('hello manage,my name is Alen!')
 print('hello Alen,my name is 经理!')
 print('mange! nice to meet you!')
