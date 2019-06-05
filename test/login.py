e186abc0b83494d0297562425f6cef4d013f48eb
经理 <xwp_fullstack@163.com>
2019-06-05 18:50:53 +0800

    manage

--- test/login.py
+++ test/login.py
@@ -1,4 +1,8 @@
 print('hello world!')
+print('hello world!')
+print('hello world!')
+print('hello world!')
+print('hello world!')
 print('hello manage,my name is Alen!')
 print('hello Alen,my name is 经理!')
 print('mange! nice to meet you!')
